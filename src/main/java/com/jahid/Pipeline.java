ed144fb179d6a75ee59af4fbc17624db71b4c08a
Jahidul Islam <jahidul479@gmail.com>
2019-05-23 13:12:32 +0600

    initial commit

--- src/main/java/com/jahid/Pipeline.java
+++ src/main/java/com/jahid/Pipeline.java
@@ -6,7 +6,7 @@ import java.util.Properties;
 
 public class Pipeline {
     private static Properties properties;
-    private static  String propertiesName = "tokenize, ssplit, pos";
+    private static  String propertiesName = "tokenize, ssplit, pos, lemma";
     private static StanfordCoreNLP stanfordCoreNLP;
 
     private Pipeline() {
